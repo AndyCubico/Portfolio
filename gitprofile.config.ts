1a24735260059916e47b9dfe78cc82bc62be0534
Andreu Nosàs Soler <99950309+AndyCubico@users.noreply.github.com>
2024-09-26 22:17:50 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -194,7 +194,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'dark',
+    defaultTheme: 'dracula',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
