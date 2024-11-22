9a909c4464d85152858741653861d88d3ef5f032
Andreu Nosàs Soler <99950309+AndyCubico@users.noreply.github.com>
2024-11-22 10:55:58 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -146,7 +146,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://scarlet-cori-53.tiiny.site/', // Empty fileUrl will hide the `Download Resume` button.
+      'https://andreu-nosas-cv.tiiny.site', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Unity',
